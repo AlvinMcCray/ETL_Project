c7ec48384e51861789e3221c40f5bc81d091f316
JB Kinlacheeny <71355546+kinlacheeny@users.noreply.github.com>
2021-01-30 11:42:01 -0500

    final push

--- Resources/ETL_Project.sql
+++ Resources/ETL_Project.sql
@@ -1,31 +1,31 @@
 -- Create tables for raw data to be loaded into
 
--- DROP TABLE players;
--- CREATE TABLE players (
--- 	player VARCHAR,
--- 	college VARCHAR,
--- 	born FLOAT,
--- 	birth_city VARCHAR, 
--- 	birth_state VARCHAR
--- 	);
+DROP TABLE players;
+CREATE TABLE players (
+	player VARCHAR,
+	college VARCHAR,
+	born FLOAT,
+	birth_city VARCHAR, 
+	birth_state VARCHAR
+	);
 
--- SELECT * FROM players;
+SELECT * FROM players;
 
--- DROP TABLE nba_twitter; 
--- CREATE TABLE nba_twitter (
--- 	player VARCHAR, 
--- 	twitter_favorite_count FLOAT,
--- 	twitter_retweet_count FLOAT
--- 	);
+DROP TABLE nba_twitter; 
+CREATE TABLE nba_twitter (
+	player VARCHAR, 
+	twitter_favorite_count FLOAT,
+	twitter_retweet_count FLOAT
+	);
 
 -- SELECT * FROM nba_twitter;
 
 -- Joins tables
--- SELECT players.player, players.college, players.born, 
--- 		players.birth_city, players.birth_state, nba_twitter.twitter_favorite_count, 
--- 		nba_twitter.twitter_retweet_count
--- FROM players
--- JOIN nba_twitter
--- ON players.player = nba_twitter.player;
+SELECT players.player, players.college, players.born, 
+		players.birth_city, players.birth_state, nba_twitter.twitter_favorite_count, 
+		nba_twitter.twitter_retweet_count
+FROM players
+JOIN nba_twitter
+ON players.player = nba_twitter.player;
 
--- SELECT * FROM nba_twitter;
+SELECT * FROM nba_twitter;
